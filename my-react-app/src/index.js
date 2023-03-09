b643ff71289f2c973f5f694b5323bf5f134730cd
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-03-09 09:53:52 -0800

    Components in Components.

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Car() {
-    return <h2>Hi, I am a Car!</h2>;
-  }
+function Car(props) {
+    return <h2>Hi, I am a {props.color} Car!</h2>;
+}
+
+function Garage() {
+    return (
+        <>
+        <h1>Yo, what's that in my garage?</h1>
+        <Car color="red"/>
+        </>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
+root.render(<Garage/>);
